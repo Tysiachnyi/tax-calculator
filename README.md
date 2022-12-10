9aaab10fb490b51caa91a519139920d249d03186
Nik Tysiachnyi <nikita.tysiachnyi@eduki.com>
2022-12-10 01:11:21 +0100

    Project init, added tailwind, eslint + rettier

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # tax-calculator
+
+This application i make for check tax on popular IT country
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App"></div>;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
